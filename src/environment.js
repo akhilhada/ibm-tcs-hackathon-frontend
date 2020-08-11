5cc4cbb257cec504eb0277703e9432b6d1366138
santhosh <santhoshramanathan25@gmail.com>
2020-08-11 16:11:04 +0530

    updated

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,4 @@
-const user_backend_url = "http://ibm-tcs-hackathon-users-backend-dev-santhosh2.sandbox-ocp43-one-621532-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud";
+const user_backend_url = "http://ibm-tcs-hackathon-users-backend-dev-santhosh2.sandbox-ocp43-one-621532-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/user";
 const hospital_backend_url = "http://ibm-tcs-hackathon-hospitals-backend-dev-santhosh2.sandbox-ocp43-one-621532-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/hospital";
 const middleware_url = "http://hackathon-middleware-dev-santhosh2.sandbox-ocp43-one-621532-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/";
 const testcenter_url = "http://ibm-tcs-hackathon-hospitals-backend-dev-santhosh2.sandbox-ocp43-one-621532-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/testcentres/";
